716c6dad930db1524cacc52033c53cdbceca02e1
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-11-20 09:26:17 -0500

    almost complete

--- Game1.cs
+++ Game1.cs
@@ -16,21 +16,28 @@ namespace topic_4_monogame_sounds
         Texture2D explosionTexture;
         Rectangle explosionRect;
 
+        Texture2D cuttersTexture;
+        Rectangle cuttersRect;
+
         SpriteFont Timefont;
         float seconds;
+
+        Rectangle greenRect, greenRect2, redRect, redRect2, redRect3;
 
         SoundEffect explosionSound;
         SoundEffectInstance explodeInstance;
 
+
         MouseState mouseState;
 
         bool done;
+        bool sound;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
@@ -42,14 +49,23 @@ namespace topic_4_monogame_sounds
 
             bombRect = new Rectangle(50, 50, 700, 400);
             explosionRect = new Rectangle(0, 0, 800, 500);
+            cuttersRect = new Rectangle(0, 0, 100, 100);
+
+            greenRect = new Rectangle(490, 160, 110, 15);
+            greenRect2 = new Rectangle(680, 180, 30, 60);
+            redRect = new Rectangle(490, 180, 100, 25);
+            redRect2 = new Rectangle(670, 170, 80, 10);
+            redRect3 = new Rectangle(715, 180, 35, 75);
+
 
             _graphics.PreferredBackBufferWidth = 800; 
             _graphics.PreferredBackBufferHeight = 500; 
             _graphics.ApplyChanges();
 
             done = false;
+            sound = false;
 
-            seconds = 5;
+            seconds = 15;
         }
 
         protected override void LoadContent()
@@ -61,6 +77,8 @@ namespace topic_4_monogame_sounds
             Timefont = Content.Load<SpriteFont>("Timefont");
 
             bombTexture = Content.Load<Texture2D>("bomb");
+
+            cuttersTexture = Content.Load<Texture2D>("cutters");
 
 
             explosionSound = Content.Load<SoundEffect>("explosion");
@@ -78,6 +96,7 @@ namespace topic_4_monogame_sounds
             // TODO: Add your update logic here
 
             mouseState = Mouse.GetState();
+            cuttersRect.Location = mouseState.Position;
             this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
             if (!done)
                 seconds -= (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -87,14 +106,62 @@ namespace topic_4_monogame_sounds
                 seconds = 0;
                 explodeInstance.Play();
                 done = true;
+                sound = true;
             }
 
-            if (done)
+            if (sound)
             {
                 if (explodeInstance.State == SoundState.Stopped) 
                     Exit();
             }
- 
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (greenRect.Contains(mouseState.Position))
+                    done = true;
+
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (greenRect2.Contains(mouseState.Position))
+                    done = true;
+
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (redRect.Contains(mouseState.Position))
+                {
+                    seconds = 0;
+                    explodeInstance.Play();
+                    done = true;
+                    sound = true;
+                }
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (redRect2.Contains(mouseState.Position))
+                {
+                    seconds = 0;
+                    explodeInstance.Play();
+                    done = true;
+                    sound = true;
+                }
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (redRect3.Contains(mouseState.Position))
+                {
+                    seconds = 0;
+                    explodeInstance.Play();
+                    done = true;
+                    sound = true;
+                }
+            }
+
 
             base.Update(gameTime);
         }
@@ -110,8 +177,10 @@ namespace topic_4_monogame_sounds
             _spriteBatch.DrawString(Timefont, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
             if (seconds <= 0)
                 _spriteBatch.Draw(explosionTexture, explosionRect, Color.White);
-
+            _spriteBatch.Draw(cuttersTexture, cuttersRect, Color.White);
             _spriteBatch.End();
+
+            
 
             base.Draw(gameTime);
         }
